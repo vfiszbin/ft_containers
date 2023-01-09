c91cb107e73a726f1d56855b7af1ec0dae18eb74
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2023-01-09 12:35:26 +0100

    Fix member type aliases and use std::vector iterator to test my vector container

--- vector.hpp
+++ vector.hpp
@@ -2,6 +2,7 @@
 # define VECTOR_HPP
 
 # include <memory>
+# include <vector> //RETIRER !!!
 
 
 namespace ft
@@ -12,12 +13,15 @@ namespace ft
 		public:
 			/// Member types ///
 			typedef T value_type;
-			typedef allocator_type Alloc;
-			typedef reference allocator_type::reference;
-			typedef const_reference allocator_type::const_reference;
-			typedef pointer allocator_type::pointer;
-			typedef const_pointer allocator_type::const_pointer;
-			//Manque tout ce qui utilise un itérateur !!!
+			typedef Alloc allocator_type;
+			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::pointer pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+			typedef typename std::vector<value_type>::iterator iterator; //Mettre mon propre iterator !
+			typedef typename std::vector<value_type>::const_iterator const_iterator; //Mettre mon propre iterator !
+			typedef typename std::vector<value_type>::template reverse_iterator<iterator> reverse_iterator; //Mettre mon propre iterator !
+			typedef typename std::vector<value_type>::template reverse_iterator<const_iterator> const_reverse_iterator; //Mettre mon propre iterator !
 			typedef ptrdiff_t difference_type; //can represent the number of element between two pointers
 			typedef size_t size_type;
 
@@ -50,7 +54,7 @@ namespace ft
 				_capacity = _size;
 				_start = _alloc.allocate(_size); //allocates a block of storage with a size large enough to contain _size elements of type value_type
 				for (difference_type i = 0; i < _size; i++)
-					_alloc.construct(_start + i, *(first + i)) //constructs (by calling the constructor of value_type with the corresponding range element as argument) an element object on the location pointed by _start + i
+					_alloc.construct(_start + i, *(first + i)); //constructs (by calling the constructor of value_type with the corresponding range element as argument) an element object on the location pointed by _start + i
 			}
 
 			//Copy constructor
@@ -81,7 +85,7 @@ namespace ft
 					if (_capacity != 0)
 						_alloc.deallocate(_start, _capacity);
 					_capacity = _size;
-					_start = _alloc.allocate(_capacity)
+					_start = _alloc.allocate(_capacity);
 				}
 
 				//Constructs the elements by copying those from x
