7b7694397a9c0fa9c0dfa9e932d0ef8afa5db5a0
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2023-01-12 11:14:03 +0100

    Add lexicographical_compare and equal implementations

--- utils.hpp
+++ utils.hpp
@@ -6,7 +6,7 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 09:18:25 by vfiszbin          #+#    #+#             */
-/*   Updated: 2023/01/12 10:45:38 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2023/01/12 11:13:38 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,77 @@ namespace ft
 	
 	template <>
 	struct is_integral<unsigned long long> : public ft::integral_constant<bool, true> {};
+
+
+	/// LEXICOGRAPHICAL_COMPARE ///
+
+	//Default implementation
+	//Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2).
+	template <class InputIterator1, class InputIterator2>
+  	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+
+	//Custom implementation : the comparison operator used is the comp function instead of <
+	//Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2).
+	template <class InputIterator1, class InputIterator2, class Compare>
+  	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1) )
+				return false;
+			else if ( comp(*first1, *first2) )
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+
+	/// EQUAL ///
+
+	//Equality implementation
+	//Compares the elements in the range [first1,last1) with those in the range beginning at first2,
+	//and returns true if all of the elements in both ranges match.
+	template <class InputIterator1, class InputIterator2>
+  	bool equal ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2 )
+	{
+		while (first1 != last1) 
+		{
+			if (!(*first1 == *first2)) 
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	}
+
+	//Predicate implementation : the elements are compared using the pred function instead of the == operator
+	//Compares the elements in the range [first1,last1) with those in the range beginning at first2,
+	//and returns true if all of the elements in both ranges match.
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1 != last1) 
+		{
+			if (!pred(*first1, *first2))
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	}
 	
 }
 
